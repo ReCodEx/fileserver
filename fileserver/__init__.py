53991187852fbd3d0e988814d32624990cf32ffe
Teyras <teyras@gmail.com>
2016-04-24 13:07:14 +0200

    register a sigint/sigterm handler in the runserver command

--- fileserver/__init__.py
+++ fileserver/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_script import Manager, Server
+import signal
 
 from .DirectoryStructure import DirectoryStructure
 
@@ -8,5 +9,16 @@ dirs = DirectoryStructure()
 
 import fileserver.views
 
+class RunServer(Server):
+    def __call__(self, app, host, port, use_debugger, use_reloader, threaded, processes, passthrough_errors):
+        def shutdown(*args, **kwargs):
+            raise KeyboardInterrupt
+
+        signal.signal(signal.SIGINT, shutdown)
+        signal.signal(signal.SIGTERM, shutdown)
+
+        super().__call__(app, host, port, use_debugger, use_reloader, threaded, processes, passthrough_errors)
+
 manager = Manager(app)
+manager.add_command("runserver", RunServer())
 
