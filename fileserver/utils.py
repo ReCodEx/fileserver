966f04a8341160acb2ca7b6e4c97d4bdbb7465fc
Teyras <Teyras@gmail.com>
2016-05-05 12:31:12 +0200

    test hash_file

--- fileserver/utils.py
+++ fileserver/utils.py
@@ -1,15 +1,18 @@
-def hash_file(hash, file_stream):
+def hash_file(hash, byte_stream):
     """
     Calculate the hash of a file by chunks (without having the whole file in memory)
     :param hash: A hash object to start with (e.g. hashlib.sha1())
-    :param file_stream: The source stream
+    :param file_stream: The source stream. It must return bytes on read (e.g. io.BytesIO).
     :return: The result hash
     """
 
     while True:
-        chunk = file_stream.read(65536)
+        chunk = byte_stream.read(65536)
 
-        if (chunk is None or chunk == "" or chunk == b""):
+        if type(chunk) is not bytes:
+            raise TypeError("only byte streams can be encoded")
+
+        if not chunk:
             break
 
         hash.update(chunk)
