2451bfc2a20e667f06de0b6abbeb0f063361be77
Teyras <teyras@gmail.com>
2016-08-26 11:18:06 +0200

    support running the fileserver with uWSGI

--- mkconfig/mkconfig.py
+++ mkconfig/mkconfig.py
@@ -15,6 +15,14 @@ apache_parser.add_argument("--working-directory", required=True, type=str, dest=
 apache_parser.add_argument("--htpasswd", type=str, dest="htpasswd", help="user file for HTTP basic auth")
 apache_parser.add_argument("--user", dest="user", help="user under which the server should be run")
 
+uwsgi_parser = subparsers.add_parser("uwsgi", help="Configure the fileserver as an uwsgi application")
+uwsgi_parser.add_argument("--user", dest="user", help="user under which the server should be run")
+uwsgi_parser.add_argument("--port", default=None, type=int, dest="port", help="port where the fileserver should listen")
+uwsgi_parser.add_argument("--socket", default=None, type=str, dest="socket", 
+                           help="path to a UNIX socket where the fileserver should listen")
+uwsgi_parser.add_argument("--working-directory", required=True, type=str, dest="working_directory",
+                           help="directory where the files should be stored")
+
 
 def die(msg):
     print(msg, file=sys.stderr)
@@ -33,6 +41,22 @@ if __name__ == "__main__":
             install_directory=os.path.dirname(script_dir),
             port=args.port,
             htpasswd=args.htpasswd,
+            user=args.user
+        ))
+    elif args.server_type == "uwsgi":
+        if args.port is None and args.socket is None:
+            die("either port or socket must be specified")
+        if args.port is not None and args.socket is not None:
+            die("port and socket cannot be used at the same time")
+
+        with open(os.path.join(script_dir, "templates", "uwsgi.ini")) as template_file:
+            template = Template(template_file.read())
+
+        print(template.render(
+            working_directory=args.working_directory,
+            install_directory=os.path.dirname(script_dir),
+            port=args.port,
+            socket=args.socket,
             user=args.user
         ))
     else:
